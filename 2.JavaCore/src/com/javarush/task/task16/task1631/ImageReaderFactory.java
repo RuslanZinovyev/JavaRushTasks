928ab4f1899df4fe51be07b9379f1f4e79e540de
Ruslan Zinovyev <ruslan.zinovyev@hotmail.com>
2018-04-27 12:25:36 +0700

    New tasks have been solved

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task16.task1631;
+
+import com.javarush.task.task16.task1631.common.*;
+
+public class ImageReaderFactory {
+
+    public static ImageReader getImageReader(ImageTypes types) {
+        if (types != null) {
+            switch (types) {
+                case JPG:
+                    return new JpgReader();
+                case BMP:
+                    return new BmpReader();
+                case PNG:
+                    return new PngReader();
+            }
+        }
+        throw new IllegalArgumentException("Неизвестный тип картинки");
+    }
+}
